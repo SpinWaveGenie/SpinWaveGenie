72e215e4a682c82388e7b556d070d2d3363d7af3
Steven Hahn <hahnse@ornl.gov>
2013-11-07 09:44:03 -0500

    new attempt at load balancing

--- src/main.cpp
+++ src/main.cpp
@@ -28,9 +28,9 @@ class ThreadClass {
 public:
     int npoints,nproc,points;
     double min,max;
-    int pointsDone;
+    int pointsDone,nextPoint;
     MatrixXd figure;
-    boost::mutex io_mutex; // The iostreams are not guaranteed to be thread-safe!
+    boost::mutex io_mutex,np_mutex; // The iostreams are not guaranteed to be thread-safe!
     ThreadClass(int n) // Constructor
     {
 #if EIGEN_WORLD_VERSION >= 3 && EIGEN_MAJOR_VERSION >= 1
@@ -39,7 +39,8 @@ public:
         nproc = n;
         npoints = 101;
         pointsDone = 0;
-        points = 101;
+        nextPoint = 0;
+        points = 81;
         figure.setZero(points,npoints);
         min = 0.0;
         max = 80.0;
@@ -55,7 +56,10 @@ public:
         SW_Builder builder = four_sl.get_builder();
         scoped_lock.unlock();
         
-        /*double x0,y0,z0,x1,y1,z1;
+        boost::unique_lock<boost::mutex> np_lock(np_mutex);
+        np_lock.unlock();
+
+        double x0,y0,z0,x1,y1,z1;
         x0=2.0;x1=2.0;
         y0=-1.5;y1=1.5;
         z0=-3.0;z1=-3.0;
@@ -64,20 +68,21 @@ public:
         resinfo.a = 1109.0;
         resinfo.b = 0.0;
         resinfo.c = 0.48;
-        resinfo.tol = 1.0e-4;
+        resinfo.tol = 1.0e-2;
         resinfo.direction = 1;
         resinfo.builder = builder;
         
         axes_info axesinfo;
         axesinfo.x = true;
-        axesinfo.y = false;
+        axesinfo.y = true;
         axesinfo.z = true;
         axesinfo.dx = 0.2;
+        axesinfo.dy = 0.05;
         axesinfo.dz = 0.2;
-        axesinfo.tol = 1.0e-4;
-        */
+        axesinfo.tol = 1.0e-2;
         
-        double x0,y0,z0,x1,y1,z1;
+        
+        /*double x0,y0,z0,x1,y1,z1;
         x0=3.0;x1=3.0;
         y0=0.0;y1=0.0;
         z0=-4.0;z1=4.0;
@@ -98,19 +103,29 @@ public:
         axesinfo.dy = 0.2;
         axesinfo.dz = 0.05;
         axesinfo.tol = 1.0e-1;
+        */
         
         scoped_lock.lock();
         double tmin = min;
         double tmax = max;
         int tpoints = points;
         int tnpoints = npoints;
-        int tnproc = nproc;
         scoped_lock.unlock();
 
         TwoDimensionResolutionFunction res(resinfo, tmin,tmax, tpoints);
         IntegrateAxes tmp(axesinfo,res,tmin,tmax,tpoints);
-        for(int m=i;m<tnpoints;m=m+tnproc)
+        //for(int m=i;m<tnpoints;m=m+tnproc)
+        while (true)
         {
+            np_lock.lock();
+            int m = nextPoint;
+            if (m > npoints)
+            {
+                break;
+            }
+            nextPoint++;
+            cout << m << endl;
+            np_lock.unlock();
             double x = x0 + (x1-x0)*m/(tnpoints-1);
             double y = y0 + (y1-y0)*m/(tnpoints-1);
             double z = z0 + (z1-z0)*m/(tnpoints-1);
