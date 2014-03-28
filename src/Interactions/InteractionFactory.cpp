9ffadb897f90f2f90f0f26057e9d497072286000
Steven Hahn <quantumsteve@gmail.com>
2014-03-28 18:13:06 -0400

    removed variatic variable from interaction factory in order to maintain compatibility with gcc 4.4

--- src/Interactions/InteractionFactory.cpp
+++ src/Interactions/InteractionFactory.cpp
@@ -14,18 +14,12 @@
 #include "AnisotropyInteraction.h"
 #include "MagneticFieldInteraction.h"
 
-template<typename T, typename ...Args>
-std::unique_ptr<T> make_unique( Args&& ...args )
-{
-    return std::unique_ptr<T>( new T( std::forward<Args>(args)... ) );
-}
-
 std::unique_ptr<Interaction> InteractionFactory::getExchange(std::string name, double value, std::string sl_r, std::string sl_s, double min, double max)
 {
     if (sl_r.compare(sl_s) == 0)
-        return std::move( make_unique<ExchangeInteractionSameSublattice>(name,value,sl_r,min,max) );
+        return std::move( std::unique_ptr<Interaction>(new ExchangeInteractionSameSublattice(name,value,sl_r,min,max)) );
     else
-        return std::move( make_unique<ExchangeInteraction>(name,value,sl_r,sl_s,min,max) );
+        return std::move( std::unique_ptr<Interaction>(new ExchangeInteraction(name,value,sl_r,sl_s,min,max)) );
 }
 
 std::unique_ptr<Interaction> InteractionFactory::getDzyaloshinskiiMoriya(std::string name, double value, Vector3 unitVector, std::string sl_r, std::string sl_s, double min, double max)
@@ -37,11 +31,11 @@ std::unique_ptr<Interaction> InteractionFactory::getDzyaloshinskiiMoriya(std::st
     
     if (x < 0.01 && y > 0.99 && z < 0.01)
     {
-        return std::move( make_unique<DM_Y_Interaction>(name,value,sl_r,sl_s,min,max) );
+        return std::move( std::unique_ptr<Interaction>( new DM_Y_Interaction(name,value,sl_r,sl_s,min,max)) );
     }
     else if (x < 0.01 && y < 0.01 && z > 0.99)
     {
-        return std::move( make_unique<DM_Z_Interaction>(name,value,sl_r,sl_s,min,max) );
+        return std::move( std::unique_ptr<Interaction>( new DM_Z_Interaction(name,value,sl_r,sl_s,min,max)) );
     }
     else
     {
@@ -52,12 +46,12 @@ std::unique_ptr<Interaction> InteractionFactory::getDzyaloshinskiiMoriya(std::st
 
 std::unique_ptr<Interaction> InteractionFactory::getAnisotropy(std::string name, double value, Vector3 unitVector, std::string sl_r)
 {
-    return std::move( make_unique<AnisotropyInteraction>(name,value,unitVector,sl_r) );
+    return std::move( std::unique_ptr<Interaction>( new AnisotropyInteraction(name,value,unitVector,sl_r)) );
 }
 
 std::unique_ptr<Interaction> InteractionFactory::getMagneticField(std::string name, double value, Vector3 unitVector, std::string sl_r)
 {
-    return std::move( make_unique<MagneticFieldInteraction>(name,value,unitVector,sl_r) );
+    return std::move( std::unique_ptr<Interaction>( new MagneticFieldInteraction(name,value,unitVector,sl_r)) );
 }
 
 
