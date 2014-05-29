fbdc8420c96cd1dd6fd46432feb13fdddbcef938
Steven Hahn <quantumsteve@gmail.com>
2014-05-29 18:28:22 -0400

    added test for HKLDirections

---
+++ test/HKLDirectionsTest.cpp
@@ -0,0 +1,13 @@
+#define BOOST_TEST_MODULE HKLDirectionsTest
+#define BOOST_TEST_MAIN
+#include <cmath>
+#include <exception>
+#include <iostream>
+#include <boost/test/unit_test.hpp>
+#include "Containers/HKLDirections.h"
+
+
+BOOST_AUTO_TEST_CASE( CheckCoefficients )
+{
+}
+
