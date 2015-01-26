b296bd6ae4ab1909d8de8c36fd0c36493115a7a4
Steven Hahn <hahnse@ornl.gov>
2015-01-26 12:08:10 -0500

    updated travis-ci to include osx

---
+++ scripts/install-deps-linux.sh
@@ -0,0 +1,7 @@
+sudo add-apt-repository --yes ppa:ubuntu-toolchain-r/test
+sudo apt-get update -qq
+sudo apt-get install -qq gcc-4.4 g++-4.4 libstdc++6-4.4-dev
+sudo apt-get install -qq libboost-dev
+sudo apt-get install -qq libboost-test-dev
+sudo apt-get install -qq libeigen3-dev
+sudo apt-get install -qq libtbb-dev
---
+++ scripts/install-deps-osx.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+brew update
+
+brew tap homebrew/science
+
+brew install cmake
+brew install graphviz
+brew install doxygen
+brew install eigen
+brew install nlopt
+brew install boost --c++11
+brew install tbb --c++11
