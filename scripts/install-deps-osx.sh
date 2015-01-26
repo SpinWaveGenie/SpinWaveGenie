15cfc7d7dcc0034f94745cb2fdad0a142b1cc215
Steven Hahn <hahnse@ornl.gov>
2015-01-26 13:30:44 -0500

    silence brew update

--- scripts/install-deps-osx.sh
+++ scripts/install-deps-osx.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-brew update --quiet
 
 brew tap homebrew/science
+# many formulas are updated, so we suppress the output.
+brew update > /dev/null 2>&1
 
 brew install --quiet eigen
 brew install --quiet nlopt
