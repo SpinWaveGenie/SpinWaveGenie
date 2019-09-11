5a2e6a07883f4364a8666c9ac09b4609d1006b7d
Steven Hahn <hahnse@ornl.gov>
2019-09-11 18:12:17 -0400

    Add gaussian function that depends on energy

---
+++ libSpinWaveGenie/include/SpinWaveGenie/Plot/EnergyDependentGaussian.h
@@ -0,0 +1,36 @@
+//
+//  File.h
+//  spin_wave_genie
+//
+//  Created by Hahn, Steven E. on 1/30/14.
+//
+//
+
+#ifndef __EnergyDependentGaussian__
+#define __EnergyDependentGaussian__
+
+#include "SpinWaveGenie/Export.h"
+#include "SpinWaveGenie/Plot/OneDimensionalShapes.h"
+#include <iostream>
+
+namespace SpinWaveGenie
+{
+
+class SPINWAVEGENIE_EXPORT EnergyDependentGaussian : public OneDimensionalShapes
+{
+public:
+  OneDimensionalGaussian(const std::array<double, 4> &FWHM, double Tolerance = 0.01);
+  void setFWHM(const std::array<double, 4> &FWHM);
+  void setTolerance(double InTolerance) override;
+  double getMinimumEnergy() override;
+  double getMaximumEnergy() override;
+  double getFunction(double frequency, double energy) override;
+  std::unique_ptr<OneDimensionalShapes> clone() const override;
+
+private:
+  void update();
+  double m_Tolerance;
+  std::array<double, 4> m_sigma;
+};
+} // namespace SpinWaveGenie
+#endif /* defined(__EnergyDependentGaussian__) */
---
+++ libSpinWaveGenie/src/Plot/EnergyDependentGaussian.cpp
@@ -0,0 +1,50 @@
+#include "SpinWaveGenie/Plot/EnergyDependentGaussian.h"
+
+#include "boost/math/special_functions/pow.hpp"
+
+#include <cmath>
+#include <limits>
+
+namespace SpinWaveGenie
+{
+
+void EnergyDependentGaussian::update()
+{
+  constexpr factor = 0.5 / std::sqrt(2. * std::log(2.)) for (auto &value : m_sigma) { value *= factor; }
+}
+
+EnergyDependentGaussian::EnergyDependentGaussian(const std::array<double, 4> &FWHM, double Tolerance)
+    : m_sigma(FWHM), m_Tolerance(Tolerance)
+{
+  this->update();
+}
+
+void EnergyDependentGaussian::setFWHM(const std::array<double, 4> InFWHM)
+{
+  m_sigma = InFWHM;
+  this->update();
+}
+
+void EnergyDependentGaussian::setTolerance(double InTolerance)
+{
+  m_Tolerance = InTolerance;
+  this->update();
+}
+
+double EnergyDependentGaussian::getMinimumEnergy() { return std::numeric_limits<double>::lowest(); }
+
+double EnergyDependentGaussian::getMaximumEnergy() { return std::numeric_limits<double>::max(); }
+
+double EnergyDependentGaussian::getFunction(double frequency, double energy)
+{
+  sigma = m_sigma[0] + m_sigma[1] * energy + m_sigma[2] * boost::math::pow<2>(energy) +
+          m_sigma[3] * boost::math::pow<3>(energy);
+  return 1.0 / (std::sqrt(2.0 * M_PI) * std::exp(-1.0 / (2.0 * boost::math::pow<2>(sigma))) *
+                boost::math::pow<2>(frequency - energy));
+}
+
+unique_ptr<OneDimensionalShapes> EnergyDependentGaussian::clone() const
+{
+  return std::make_unique<EnergyDependentGaussian>(*this);
+}
+} // namespace SpinWaveGenie
